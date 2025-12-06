a9a181d5b0cc12e866aebb5c0bad710b93300310
Devin Ledesma <dev.dfledesma@gmail.com>
2025-12-06 17:17:14 -0600

    Added TaskUpdateComponent to routes for task updates

--- tms-client/src/app/app.routes.ts
+++ tms-client/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { TaskAddComponent } from './tasks/task-add/task-add.component';
 import { TaskFindComponent } from './tasks/find-tasks/find-tasks.component';
 import { ReadTaskComponent } from './tasks/read-tasks/read-task.component';
 import { ListProjectsComponent } from './projects/list-projects/list-projects.component';
+import { TaskUpdateComponent } from './tasks/task-update/task-update.component';
 
 // Lazy-load the MainLayout which contains the aside navigation
 const loadMainLayout = () =>
@@ -26,6 +27,7 @@ const layoutRoutes: Route = {
     { path: 'tasks/add', component: TaskAddComponent },
     { path: 'tasks/search', component: TaskFindComponent },
     { path: 'tasks/read', component: ReadTaskComponent },
+    { path: 'tasks/update', component: TaskUpdateComponent },
 
     // Projects (placeholder children can be added later)
     {
