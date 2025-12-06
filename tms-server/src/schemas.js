771d20f9464be76ef6c7de95db54357a6d46e006
Devin Ledesma <dev.dfledesma@gmail.com>
2025-12-06 15:46:18 -0600

    Added `updateTaskSchema` for validating task updates.

--- tms-server/src/schemas.js
+++ tms-server/src/schemas.js
@@ -18,4 +18,23 @@ const addTaskSchema = {
     additionalProperties: false,
 };
 
-module.exports = { addTaskSchema };
+const updateTaskSchema = {
+    type: "object",
+    properties: {
+        title: { type: "string", minLength: 3, maxLength: 100 },
+        description: { type: "string", maxLength: 500 },
+        status: { type: "string", enum: ["Pending", "In Progress", "Completed"] },
+        priority: { type: "string", enum: ["Low", "Medium", "High"] },
+        dueDate: {
+            type: "string",
+            pattern: "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}Z$",
+        }
+    },
+    required: ["title", "status", "priority"],
+    additionalProperties: false,
+}
+
+module.exports = {
+    addTaskSchema,
+    updateTaskSchema
+};
