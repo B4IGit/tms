69fe30593d592c89d9c8c5251e46d5e57027a69f
Devin Ledesma <dev.dfledesma@gmail.com>
2025-12-07 23:45:29 -0600

    Added unit tests for `TaskUpdateComponent` to cover form control behavior and validation scenarios.

--- tms-client/src/app/tasks/task-update/task-update.component.spec.ts
+++ tms-client/src/app/tasks/task-update/task-update.component.spec.ts
@@ -1,4 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 import { TaskUpdateComponent } from './task-update.component';
 
@@ -8,7 +10,7 @@ describe('TaskUpdateComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaskUpdateComponent]
+      imports: [TaskUpdateComponent, HttpClientTestingModule, RouterTestingModule],
     })
     .compileComponents();
 
@@ -19,5 +21,27 @@ describe('TaskUpdateComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set form controls as disabled', () => {
+    expect(component.taskForm.get('title')?.disabled).toBeTrue();
+    expect(component.taskForm.get('status')?.disabled).toBeTrue();
+    expect(component.taskForm.get('priority')?.disabled).toBeTrue();
+  });
+
+  it('should enable edit controls when setEditControlsEnabled(true) is called', () => {
+    component.setEditControlsEnabled(true);
+    expect(component.taskForm.get('title')?.enabled).toBeTrue();
+    expect(component.taskForm.get('description')?.enabled).toBeTrue();
+    expect(component.taskForm.get('dueDate')?.enabled).toBeTrue();
+  });
+
+
+  it('should invalidate title shorter than 3 characters', () => {
+    const titleControl = component.taskForm.get('title');
+    titleControl?.enable();
+    titleControl?.setValue('ab');
+    expect(titleControl?.invalid).toBeTrue();
+    expect(titleControl?.errors?.['minlength']).toBeTruthy();
   });
 });
